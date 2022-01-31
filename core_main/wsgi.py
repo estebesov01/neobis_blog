9566dba1c5b80df10a511ef14058678530f50af1
estebesov01 <estebesov7777@gmail.com>
2022-02-01 00:37:40 +0600

    fixes

--- core_main/wsgi.py
+++ core_main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mele_blog project.
+WSGI config for core_main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mele_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_main.settings')
 
 application = get_wsgi_application()
